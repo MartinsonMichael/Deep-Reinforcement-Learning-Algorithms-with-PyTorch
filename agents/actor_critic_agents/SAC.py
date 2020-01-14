ba68a7278cd7157b9ca05f9820f7dbe51dc5429a
Martinson Michael <mmartinson@yandex.ru>
2020-01-14 17:22:45 +0300

    add script for docker launch

--- agents/actor_critic_agents/SAC.py
+++ agents/actor_critic_agents/SAC.py
@@ -133,9 +133,6 @@ class SAC(Base_Agent):
         eval_ep = self.episode_number % TRAINING_EPISODES_PER_EVAL_EPISODE == 0 and self.do_evaluation_iterations
         self.episode_step_number_val = 0
         while not self.done:
-
-            print('\n\nstep\n')
-
             self.episode_step_number_val += 1
             self.action = self.pick_action(eval_ep)
             self.conduct_action(self.action)
