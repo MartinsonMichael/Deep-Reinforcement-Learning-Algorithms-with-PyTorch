280d1331c13f8670c509a0797bd3a635fe556864
Martinson Michael <mmartinson@yandex.ru>
2020-01-23 18:40:09 +0300

    fix

--- entrypoint.py
+++ entrypoint.py
@@ -80,7 +80,7 @@ def create_config(args):
                 "gradient_clipping_norm": 5,
                 "initialiser": "Xavier"
             },
-            "save_frequency_episode": 3000,
+            "save_frequency_episode": 500,
             "min_steps_before_learning": 50000,
             "batch_size": 128,
             "discount_rate": 0.99,
